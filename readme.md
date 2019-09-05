d96ec439de820a0d194c0eda477f6f66b40a2b7a
王冬 <20004604@qq.com>
2019-09-05 15:25:43 +0800

    update

--- readme.md
+++ readme.md
@@ -1,3 +1,17 @@
 # react脚手架
 
-目前包含eslint等多种东西，但React全家桶里只有React本身
+react脚手架，包含：
+
+* antd
+* axios
+* autoprefixer（增加css兼容性后缀）
+* babel
+* eslint
+* less
+* postcss
+* webpack3
+* uglifyjs-webpack-plugin（代码压缩混淆）
+
+React全家桶里只有React本身
+
+多入口放在 src/page 目录下，见文件注释
--- src/page/test/app.jsx
+++ src/page/test/app.jsx
@@ -1,11 +1,13 @@
 import {DatePicker} from 'antd'; // 加载 JS
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style.less';
 
 class Root extends React.Component {
     render () {
         return <div>
             <DatePicker/>
+            <button id="abc">abcde</button>
         </div>;
     }
 }
